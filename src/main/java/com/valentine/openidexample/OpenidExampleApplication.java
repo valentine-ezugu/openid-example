4b072b14dd958e388382661d5f4a5dd407408bfc
Valentine Ezugu <valentine.ezugu@autokonto.pl>
2021-04-12 21:25:05 +0200

    first commit

---
+++ src/main/java/com/valentine/openidexample/OpenidExampleApplication.java
@@ -0,0 +1,13 @@
+package com.valentine.openidexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenidExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenidExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valentine/openidexample/OpenidExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentine.openidexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenidExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
